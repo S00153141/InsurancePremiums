25a886230f07a8fc18f88a88b6de161f3e0ba093
S00153141 <S00153141@mail.itsligo.ie>
2017-02-28 16:57:04 +0000

    Working with Fitnesse

---
+++ Premiums/PremiumsTests/PremiumsFixture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using fit;
+using Premiums;
+
+namespace PremiumsTests
+{
+    public class PremiumsFixture: fit.ColumnFixture
+    {
+        public int age;
+        public string gender;
+
+        CalculatePremiums cal = new CalculatePremiums();
+
+        public float GetPremium()
+        {
+            return cal.CalculatePremium(gender, age);
+        }
+    }
+}
